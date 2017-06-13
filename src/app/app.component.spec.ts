96caf78717bff7d0332cd379c89c8335410097f6
Michael Syme <m_dsyme@yahoo.com.au>
2017-06-13 12:13:43 +1000

    Added tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -29,4 +29,175 @@ describe('AppComponent', () => {
     const compiled = fixture.debugElement.nativeElement;
     expect(compiled.querySelector('h1').textContent).toContain('Angular Calculator');
   }));
+
+  it('should render an error when an incomplete equation is entered (Input: 1).', async(() => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    var compiled = fixture.debugElement.nativeElement;
+    compiled.querySelector('input').value = "1";
+    fixture.detectChanges();
+    expect(compiled.querySelector('h3').textContent).toContain('Please enter equations in operand operation operand format separated by space.');
+  }));
+
+  it('should render an error when an incomplete equation is entered (Input 1 +).', async(() => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    var compiled = fixture.debugElement.nativeElement;
+    compiled.querySelector('input').value = "1 +";
+    fixture.detectChanges();
+    expect(compiled.querySelector('h3').textContent).toContain('Please enter equations in operand operation operand format separated by space.');
+  }));
+
+  it('should render an error when an incorrect equation is entered (Input 1 + +).', async(() => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    var compiled = fixture.debugElement.nativeElement;
+    compiled.querySelector('input').value = "1 + +";
+    fixture.detectChanges();
+    expect(compiled.querySelector('h3').textContent).toContain('Invalid input pattern. Detected 2 operations together.');
+  }));
+
+  it('should render an error when an incorrect equation is entered (Input 1 1 +).', async(() => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    var compiled = fixture.debugElement.nativeElement;
+    compiled.querySelector('input').value = "1 1 +";
+    fixture.detectChanges();
+    expect(compiled.querySelector('h3').textContent).toContain('Invalid input pattern. Detected 2 operands together.');
+  }));
+
+  it('should render an error when an incorrect operand character is entered (Input 1 + b).', async(() => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    var compiled = fixture.debugElement.nativeElement;
+    compiled.querySelector('input').value = "1 + b";
+    fixture.detectChanges();
+    expect(compiled.querySelector('h3').textContent).toContain('invalid operand or operation detected - b');
+  }));
+
+  it('should render an error when an incorrect operand character is entered (Input 1 = 1).', async(() => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    var compiled = fixture.debugElement.nativeElement;
+    compiled.querySelector('input').value = "1 = 1";
+    fixture.detectChanges();
+    expect(compiled.querySelector('h3').textContent).toContain('invalid operand or operation detected - =');
+  }));
+
+  it('Valid Equation (Input 1 + 10).', async(() => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    var compiled = fixture.debugElement.nativeElement;
+    compiled.querySelector('input').value = "1 + 10";
+    fixture.detectChanges();
+    expect(compiled.querySelector('h3').textContent).toContain('Result: 11');
+  }));
+
+  it('Valid Equation (Input 1 - 1).', async(() => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    var compiled = fixture.debugElement.nativeElement;
+    compiled.querySelector('input').value = "1 - 1";
+    fixture.detectChanges();
+    expect(compiled.querySelector('h3').textContent).toContain('Result: 0');
+  }));
+
+  it('Valid Equation (Input 1 - 2).', async(() => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    var compiled = fixture.debugElement.nativeElement;
+    compiled.querySelector('input').value = "1 - 2";
+    fixture.detectChanges();
+    expect(compiled.querySelector('h3').textContent).toContain('Result: -1');
+  }));
+
+  it('Valid Equation (Input 3 * 5).', async(() => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    var compiled = fixture.debugElement.nativeElement;
+    compiled.querySelector('input').value = "3 * 5";
+    fixture.detectChanges();
+    expect(compiled.querySelector('h3').textContent).toContain('Result: 15');
+  }));
+
+  it('Valid Equation (Input 6 / 3).', async(() => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    var compiled = fixture.debugElement.nativeElement;
+    compiled.querySelector('input').value = "6 / 3";
+    fixture.detectChanges();
+    expect(compiled.querySelector('h3').textContent).toContain('Result: 2');
+  }));
+
+  it('Valid Equation (Input 3 / 6).', async(() => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    var compiled = fixture.debugElement.nativeElement;
+    compiled.querySelector('input').value = "3 / 6";
+    fixture.detectChanges();
+    expect(compiled.querySelector('h3').textContent).toContain('Result: 0.5');
+  }));
+
+  it('Valid Equation (Input 3 / 0).', async(() => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    var compiled = fixture.debugElement.nativeElement;
+    compiled.querySelector('input').value = "3 / 0";
+    fixture.detectChanges();
+    expect(compiled.querySelector('h3').textContent).toContain('Result: Infinity');
+  }));
+
+  it('Valid Equation (Input 3 ^ 6).', async(() => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    var compiled = fixture.debugElement.nativeElement;
+    compiled.querySelector('input').value = "3 ^ 6";
+    fixture.detectChanges();
+    expect(compiled.querySelector('h3').textContent).toContain('Result: 729');
+  }));
+
+  it('Valid Equation (Input 3000000 + 6000000).', async(() => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    var compiled = fixture.debugElement.nativeElement;
+    compiled.querySelector('input').value = "3000000 + 6000000";
+    fixture.detectChanges();
+    expect(compiled.querySelector('h3').textContent).toContain('Result: 9000000');
+  }));
+
+  it('Valid Equation (Input 3 + 6 + 9).', async(() => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    var compiled = fixture.debugElement.nativeElement;
+    compiled.querySelector('input').value = "3 + 6 + 9";
+    fixture.detectChanges();
+    expect(compiled.querySelector('h3').textContent).toContain('Result: 18');
+  }));
+
+  it('Valid Equation (Input 3 + 6 - 9).', async(() => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    var compiled = fixture.debugElement.nativeElement;
+    compiled.querySelector('input').value = "3 + 6 - 9";
+    fixture.detectChanges();
+    expect(compiled.querySelector('h3').textContent).toContain('Result: 0');
+  }));
+
+  it('Valid Equation (Input 3 * 3 / 9).', async(() => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    var compiled = fixture.debugElement.nativeElement;
+    compiled.querySelector('input').value = "3 * 3 / 9";
+    fixture.detectChanges();
+    expect(compiled.querySelector('h3').textContent).toContain('Result: 1');
+  }));
+
+  it('Valid Equation (Input 1 + 1 + 2 * 5).', async(() => {
+    const fixture = TestBed.createComponent(AppComponent);
+    fixture.detectChanges();
+    var compiled = fixture.debugElement.nativeElement;
+    compiled.querySelector('input').value = "1 + 1 + 2 * 5";
+    fixture.detectChanges();
+    expect(compiled.querySelector('h3').textContent).toContain('Result: 20');
+  }));
 });
